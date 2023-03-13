041fe2c10a87193c3b777105d07733da313ace0e
Quinterlozi <jojobobo254@gmail.com>
2023-03-13 23:04:54 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+let multiply = num1 * num2;
+
+console.log(multiply)
+
+let random = 9;
+console.log (random)
+
+let num3 = 22;
+let num4 = 9;
+
+let mod = num3 % num4;
+
+console.log(mod);
+
+
+let numbers = [7, 12, 3, 20, 9, 5];
+
+let max = Math.max(...numbers);
+
+console.log(max);
+
+
